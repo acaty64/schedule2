497be77e6e8a34af9b8e66f86e2d57f6fa53b317
vagrant <vagrant@homestead>
2019-12-14 12:36:11 -0500

    Bootstrap 3.4

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -7,8 +7,9 @@
 		<div>Tfno. 533-0008 anexo 250</div>
 	</span>	
 	<span  class="nav navbar-nav list-group-item list-inline" style="color:blue; font-size:75%">
-		<div>Laravel v5.5</div>
+		<div>Laravel v6.0</div>
 		<div>Vue js v2.5</div>
+		<div>Vuex v3.1</div>
 	</span>	
     <div class="nav navbar-nav list-group-item list-inline" id="userType" style="color:red; font-size:75%">
     	<div>ID de usuario: {{ \Auth::user()->id }}</div>
